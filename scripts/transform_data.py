2f707451b5e7184d6027a3d86c650d5dc1cfc1d2
PhaelLima-j <raphael.rodrigues.58010@a.fecaf.com.br>
2025-05-30 08:11:06 -0300

    Criando meu script

--- scripts/transform_data.py
+++ scripts/transform_data.py
@@ -1,1 +1,66 @@
-a
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
+import requests
+import pandas as pd
+import os
+from dotenv import load_dotenv
+
+class TransformData:
+
+    def __init__ (self, uri, db_name, col_name):
+        self.uri = uri
+        self.client = None
+        self.db_name = db_name
+        self.col_name = col_name
+        self.collection = None
+        self.db = None
+
+    def connect(self):
+        self.client = MongoClient (self.uri, server_api=ServerApi('1'))
+        try:
+            self.client.admin.command('ping')
+            print('Conectado ao MongoDB!')
+        except Exception as e:
+            print(f"Erro ao conectar: {e}")
+
+    def create_connect_db (self):
+        self.db = self.client[self.db_name]
+    
+    def create_connect_collection (self):
+        self.collection = self.db[self.col_name]
+
+    def visualize_collection (self):
+        for doc in self.collection.find():
+            print(doc)
+    
+    def rename_column (self, col_name, new_name):
+        self.collection.update_many({}, {"$rename": {col_name: new_name}})
+        return self.collection.find_one()
+    
+    def select_category (self, category):
+        cursor = self.collection.find({"Categoria do Produto": f"{category}"})
+        lista_specific_category = []
+        for doc in cursor:
+            lista_specific_category.append(doc)
+        return lista_specific_category
+    
+    def make_regex (self, regex):
+        query = {"Data da Compra": {"$regex": f"{regex}"}}
+
+        lista_regex = []
+        for doc in self.collection.find(query):
+            lista_regex.append(doc)
+
+        return lista_regex
+
+    def create_dataframe (self, lista):
+        df = pd.DataFrame(lista)
+        return df
+
+    def format_date(self, df):
+        df["Data da Compra"] = pd.to_datetime(df["Data da Compra"], format="%d/%m/%Y")
+        df["Data da Compra"] = df["Data da Compra"].dt.strftime("%Y-%m-%d")
+
+    def save_csv(self, df, path):
+        df.to_csv(path, index=False)
+   
